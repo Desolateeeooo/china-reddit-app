f4f7da4e8ad4ec7d72b28b04ba6c88bd589a24d8
Ivan Horidko <yellowthunder873@gmail.com>
2025-09-03 14:59:17 +0500

    Create Logo.tsx

---
+++ src/components/Navigation/Logo.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+function Logo() {
+	return (
+		<div>
+			<Image 
+				src="logo.png"
+				alt="China-Reddit Logo"
+			/>
+		</div>
+	)
+}
+
+export default Logo
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,4 +1,6 @@
 
+
+
 function Navigation() {
 	return (
 		<div>
